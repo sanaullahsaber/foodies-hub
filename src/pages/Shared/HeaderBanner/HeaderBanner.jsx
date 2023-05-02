7316ea6a1cc4f373578ef547fb5f3aca437e28ef
sanaullahsabere <you@example.com>
2023-05-02 11:45:18 +0600

    first commit

---
+++ src/pages/Shared/HeaderBanner/HeaderBanner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+
+import biryani from "../../../assets/img/food/chicken-biryani.png";
+import paratha from "../../../assets/img/food/paratha.png";
+import somosa from "../../../assets/img/food/samosa.png";
+import tea from "../../../assets/img/food/tea.png";
+
+const HeaderBanner = () => {
+  return (
+    <Container className="mt-5 ">
+      
+    </Container>
+  );
+};
+
+export default HeaderBanner;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: ''
+      }
+    ]
+  }
+])
+
+export default router;
