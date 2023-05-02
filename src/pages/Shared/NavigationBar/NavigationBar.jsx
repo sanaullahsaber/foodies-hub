d61adacda25ae691cca4c1d3851341e90da2caca
sanaullahsabere <you@example.com>
2023-05-02 11:46:49 +0600

    navbar added

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -10,7 +10,33 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div style={{ backgroundColor: "black" }}>
-      
+      <Container>
+        <Navbar expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">
+              <Link
+                className="text-success text-decoration-none fs-3 fw-bold"
+                to="/"
+              >
+                F<span className="text-light">oo</span>diesHub
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="ms-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link className="text-light text-decoration-none fs-3 fw-bold" to="/">Home</Nav.Link>
+                <Nav.Link className="text-light text-decoration-none fs-3 fw-bold" to="/about">About Us</Nav.Link>
+                <Nav.Link className="text-light text-decoration-none fs-3 fw-bold" to="/blog">Blog</Nav.Link>
+                <Nav.Link className="text-light text-decoration-none fs-3 fw-bold" to="/contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
     </div>
   );
 };
