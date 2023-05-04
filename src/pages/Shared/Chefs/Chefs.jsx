a51b192af6aba7f8ed3987d790030c0ab82ebfb8
sanaullahsabere <you@example.com>
2023-05-04 15:41:55 +0600

    server site hosting in vercel

--- src/pages/Shared/Chefs/Chefs.jsx
+++ src/pages/Shared/Chefs/Chefs.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Col, Container, Row } from 'react-bootstrap';
-import ChefsCard from './ChefsCard';
+import { Col, Container, Row } from "react-bootstrap";
+import ChefsCard from "./ChefsCard";
 
 const Chefs = () => {
   const [chefs, setChefs] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/chefs")
-      .then(res => res.json())
-      .then(data => setChefs(data))
-    .catch( error=>console.error(error))
-  },[])
+    fetch("https://foodies-hub-server-sanaullahsaber.vercel.app/chefs")
+      .then((res) => res.json())
+      .then((data) => setChefs(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <Container>
