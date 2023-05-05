b678bb05a767062504eb9032c6fe4a22c19bf02e
sanaullahsabere <you@example.com>
2023-05-05 17:50:08 +0600

    login page change the color

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
@@ -68,53 +68,62 @@ const Login = () => {
   };
 
   return (
-    <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-      <h1 className="text-center mb-5">Welcome back!</h1>
-      <Form onSubmit={handleLogin} className="w-50">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" className="w-100">
-          Login
-        </Button>
-        <Form.Text className="mt-3 text-secondary">
-          Don't have an account yet?{" "}
-          <Link to="/register" className="text-decoration-none">
-            Register now
-          </Link>
-        </Form.Text>
-        <Button
-          onClick={handleGoogleSignIn}
-          variant="primary"
-          type="submit"
-          className="w-100 mt-1 p-1"
-        >
-          <FaGoogle></FaGoogle>oogle
-        </Button>
-        <Button
-          onClick={handleGithubSignIn}
-          variant="primary"
-          type="submit"
-          className="w-100 mt-1 p-1"
-        >
-          <FaGithub></FaGithub>GitHub
-        </Button>
-      </Form>
+    <Container className="d-flex align-items-center justify-content-center min-vh-100">
+      <Card
+        className="shadow-lg border-0 rounded-3"
+        style={{ maxWidth: "400px" }}
+      >
+        <Card.Body>
+          <h1 className="text-center mb-5">Welcome back!</h1>
+          <Form onSubmit={handleLogin}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+            </Form.Group>
+            <Button variant="outline-warning" type="submit" className="w-100">
+              Login
+            </Button>
+            <Form.Text className="mt-3 text-muted">
+              Don't have an account yet?{" "}
+              <Link to="/register" className="text-decoration-none fw-bold">
+                Register now
+              </Link>
+            </Form.Text>
+            <Button
+              onClick={handleGoogleSignIn}
+              variant="outline-warning"
+              type="submit"
+              className="w-100 mt-3 d-flex align-items-center justify-content-center"
+            >
+              <FaGoogle className="me-2" />
+              Login with Google
+            </Button>
+            <Button
+              onClick={handleGithubSignIn}
+              variant="outline-warning"
+              type="submit"
+              className="w-100 mt-3 d-flex align-items-center justify-content-center"
+            >
+              <FaGithub className="me-2" />
+              Login with GitHub
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
